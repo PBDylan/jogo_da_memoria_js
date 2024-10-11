ac479ed927e2d439f27563a5666c4302937a4272
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-10-11 08:36:19 -0300

    Memória variável construida e animações das cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -23,5 +23,20 @@ for(let i=0; i < emojis.length; i++)
     ("div");
     box.className = "item";
     box.innerHTML = shuffle_emojis[i];
+    box.onclick = handle_click;
     document.querySelector(".game").appendChild(box);
+}
+
+function handle_click() {
+    if(open_cards.length <2){
+        this.class_list.add("box_open");
+        open_cards.push(this);
+    }
+    if(open_cards.lenght == 2){
+        setTimeout(check_math, 500);
+    }
+}
+
+function check_math() {
+    if(open_cards[0].innerHTML ===)
 }
